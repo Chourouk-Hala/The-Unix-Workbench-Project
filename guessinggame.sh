f1e031b551ea41e50b9bdcc26e2ac0d0469d051e
Chourouk-Hala <shinhala28@email.com>
2020-09-12 15:20:05 +0100

    finished the bash

---
+++ guessinggame.sh
@@ -0,0 +1,22 @@
+echo "counting..."
+
+num=$(ls | wc -w)
+
+echo "How many files do you think  are in the current directory"
+echo "Please take a guess"
+
+while read n
+do
+  if [[ $n -eq $num ]]
+	then
+  		break
+  elif [[ $n -lt $num ]]
+	then
+  		echo "Guess again but Higher pls"
+  else
+  		echo "Guess again but Lower pls"
+  fi
+done
+
+echo "congratulations you did it"
+echo "we have a winner"
